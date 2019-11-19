b247640f89f1fb46fc077dc2bc56d31d38dd781b
austinfulks <54602734+austinfulks@users.noreply.github.com>
2019-11-19 15:05:11 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var description = document.getElementById("description")
+
+function honey() {
+	description.innerHTML = "Badgers are thick set, round backed mammals that are very powerful for their size. Badgers have course, grey body fur and black coloured short, but powerful legs and their paws are adapted with long, sharp claws that are very useful for digging."
+}
+function weasel() {
+	description.innerHTML = "Weasels have a slim, elongated body with a small, flattish, triangular head. They have small, rounded ears but large eyes and a pointy snout with long whiskers. Their back is usually brown, gray, or black, but they have a white or yellow chin and belly. Males are much larger than the females."
+}
+function badger() {
+	description.innerHTML = "A ferret is a small, furry creature with a cone-shaped nose, long tail and a long, pear-shaped body with short legs and long claws. Ferrets are related to wolverines, ermines, minks and weasels in the Mustela genus. They are popular, though often controversial, pets. ... There are no naturally wild domesticated ferrets.."
+}
+function austin() {
+	description.innerHTML = " The American mink is a furry animal with dark brown to black fur and a long tail with a slender body and short, stubby legs. It has webbed feet and an inner layer of soft underfur with an outer layer of oily, water-resistant fur."
+}
+function marten() {
+	description.innerHTML = " The American marten is a long, slender-bodied weasel about the size of a mink with relatively large rounded ears, short limbs, and a bushy tail. American marten have a roughly triangular head and sharp nose. Their long, silky fur ranges in color from pale yellowish buff to tawny brown to almost black. Their head is usually lighter than the rest of their body, while the tail and legs are darker. American marten usually have a characteristic throat and chest bib ranging in color from pale straw to vivid orange.. "
+}
+function wolv() {
+	description.innerHTML = " The wolverine is a powerful animal that resembles a small bear but is actually the largest member of the Mustelidae (weasel) family. ....Wolverines have poor eyesight, but their senses of hearing and smell are excellent, which help them find the prey more easily."
+}
+function reset(){
+	window.location.reload();
+}
